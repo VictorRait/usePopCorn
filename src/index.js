aabfdd1382046e5d37f637a05ccf77b2ca747c09
VictorRait <victorlemuelrait@gmail.com>
2023-09-04 18:00:45 +0800

    last try

--- src/index.js
+++ src/index.js
@@ -2,17 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App';
-// import StarRating from "./StarRating";
-
-// function Test(){
-//   const [rating, setRating] = useState(0)
-//   return <div>
-//     <StarRating color="pink" onRating ={setRating}>
-
-//     </StarRating>
-//     <p>The movie was rated {rating}</p>
-//   </div>
-// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
